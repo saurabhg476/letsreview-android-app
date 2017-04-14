b94ea5578e333175172ac47bda6e0ea38206eef6
saurabh gupta <saurabh.gupta@novopay.in>
2017-04-14 17:01:51 +0530

    showing search results on home activity only

--- MyApplication/app/src/main/java/com/project/letsreview/ui/HomeActivity.java
+++ MyApplication/app/src/main/java/com/project/letsreview/ui/HomeActivity.java
@@ -1,27 +1,56 @@
 package com.project.letsreview.ui;
 
 import android.app.SearchManager;
-import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.project.letsreview.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ComponentName cn = new ComponentName(this, SearchableActivity.class);
+        listView = (ListView) findViewById(R.id.listview_topics);
 
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-
         SearchView searchView = (SearchView) findViewById(R.id.search);
         searchView.setIconifiedByDefault(false);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(cn));
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+    }
+
+    private void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void doMySearch(String query){
+        List<String> list = new ArrayList<>();
+        list.add(query + "1");
+        list.add(query + "2");
+        list.add(query + "3");
+        list.add(query + "4");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item_topic,R.id.list_item_topic_textview,list);
+        listView.setAdapter(adapter);
     }
 }
